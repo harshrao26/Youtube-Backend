5e37f8c0a1848a53ec70c616935d763301e1b286
Harsh Deep Rao <95770833+harshrao26@users.noreply.github.com>
2024-09-19 07:13:47 +0000

    Utils Added

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,37 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './src/db/index.js'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 const app = express()
-
 dotenv.config({
     path: './.env'
 })
 
-app.get('/', (req, res)=>{
-    res.send("<h1>Home</h1>")
-}
-)
+app.use(cors({
+    origin: process.env.CORS,
+    credential: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true,
+    limit: "8kb"
+}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
 connectDB()
-app.listen(process.env.PORT, ()=>{
-    console.log(`App is Listening on: http://localhost:${process.env.PORT}`)
-})
+.then(
+    ()=>{
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is Listening on: http://localhost:${process.env.PORT}`)
+        })
+    }
+)
+.catch(
+    (err)=>{
+        console.log("APP.js File Error", err)
+    }
+)
+
