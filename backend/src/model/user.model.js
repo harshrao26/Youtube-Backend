c9b38ef84358a12bbf910d239f377b4f74aa5828
Harsh Deep Rao <95770833+harshrao26@users.noreply.github.com>
2024-09-19 07:29:34 +0000

    User Model Created

---
+++ backend/src/model/user.model.js
@@ -0,0 +1,51 @@
+import mongoose, { Mongoose } from 'mongoose'
+
+const UserSchema = new Mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        index: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: true
+    },
+    fullname: {
+        type: String,
+        required: true
+    },
+    avatarImg: {
+        type: String
+    },
+    coverImg:{
+        type: String
+    },
+    watchHistory: 
+        [
+            {            
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    refreshToken: {
+        type: String
+    }
+    
+    
+    
+}, {
+    timestamps: true
+})
+
+export const User = mongoose.model("User", UserSchema)
