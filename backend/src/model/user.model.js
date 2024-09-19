3369a54d618b0bc6fc8cbf5628b882b552f12b7a
Harsh Deep Rao <95770833+harshrao26@users.noreply.github.com>
2024-09-19 10:13:42 +0000

    Access & Refresh Token Added

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
-
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+import bcrypt from "bcrypt"
 const UserSchema = new Schema({
     username: {
         type: String,
@@ -47,5 +48,49 @@ const UserSchema = new Schema({
 }, {
     timestamps: true
 })
+
+UserSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)  
+    next()
+
+})
+
+UserSchema.methods.isPasswordCorrect = async function (password){
+    return bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.generateAccessToken = function (){
+    return jwt.sign ({
+        _id: this._id,
+        fullname: this.fullname,
+        email: this.email,
+        username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expireIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+
+)
+
+}
+
+UserSchema.methods.generateRefreshToken = function (){
+    return jwt.sign ({
+        _id: this._id,
+   
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expireIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+
+)
+
+}
+
+
+
 
 export const User = mongoose.model("User", UserSchema)
